fa13e54bdc9fb21c55fdadb4e6510df564447c3d
hshehabu <hamzashehabu457@gmail.com>
2024-10-10 10:24:20 +0300

    this is how it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { createGlobalState } from 'react-hooks-global-state';
 
+const initialState = { count: 0 };
+const { useGlobalState } = createGlobalState(initialState);
+
+const Counter = () => {
+  const [count, setCount] = useGlobalState('count');
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <span>Counter: {count}</span>
+      <button onClick={() => setCount(v => v + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+    </div>
+  );
+};
+
+const App = () => (
+  <>
+    <Counter />
+    <Counter />
+  </>
+);
 
 export default App
